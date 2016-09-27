f0e363e0368881da55733ef2d5aceb4380f2a51a
devjanaprime <dev@primeacademy.io>
2016-09-26 19:08:17 -0500

    added basic checking functionality

--- public/scripts/authExample.js
+++ public/scripts/authExample.js
@@ -1,10 +1,68 @@
-console.log( 'js' );
+// var lock = new Auth0Lock( 'CLIENTID', 'DOMAIN');
+var lock = new Auth0Lock( 'FzlbLwW024XmqaSV8OjpkObvSEJUbOvl', 'devjanaprime.auth0.com');
+// log out url, from Auth0
+var logOutUrl = 'https://devjanaprime.auth0.com/v2/logout';
+
 var myApp=angular.module( 'myApp', [] );
 
-myApp.controller( 'authController', [ '$scope', function( $scope ){
-  console.log( 'authController here!');
-  $scope.logInButton = function(){
+// basic controller
+myApp.controller( 'authController', [ '$scope', '$http', function( $scope, $http ){
+
+  //run at controller load
+  $scope.init = function(){
+    console.log( 'in init' );
+    // check if a user's info is saved in localStorage
+    if( JSON.parse( localStorage.getItem( 'userProfile' ) ) ){
+      // if so, save userProfile as $scope.userProfile
+      $scope.userProfile = JSON.parse( localStorage.getItem( 'userProfile' ) );
+      console.log( 'loggedIn:', $scope.userProfile );
+      $scope.showUser = true;
+    }
+    else{
+      // if not, make sure we are logged out and empty
+      emptyLocalStorage();
+      $scope.showUser = false;
+    }
+  } // end init function
+
+  $scope.logIn = function(){
     // call out logIn function from auth0.js
-    logIn();
+    console.log( 'in logIn' );
+    lock.show( function( err, profile, token ) {
+      if (err) {
+        console.error( "auth error: ", err);
+      } // end error
+      else {
+        // save token to localStorage
+        localStorage.setItem( 'userToken', token );
+        // save user profile to localStorage
+        localStorage.setItem( 'userProfile', JSON.stringify( profile ) );
+        // reload page because dirtyhaxorz
+        location.reload();
+      } // end no error
+    }); //end lock.show
   }; // end scope.logIn
+
+  $scope.logOut = function(){
+    // call our logOutUrl
+    $http({
+      method:'GET',
+      url: logOutUrl,
+    }).then( function( data ){
+      // if logged out OK
+      if( data.data == 'OK' ){
+        // empty localStorage
+        emptyLocalStorage();
+        $scope.showUser = false;
+      }
+    })
+  }; // end scope.logIn
+
+  // run init on controller load
+  $scope.init();
 }]); // end authController
+
+var emptyLocalStorage = function(){
+  localStorage.removeItem( 'userProfile' );
+  localStorage.removeItem( 'userToken' );
+}; // end emptyLocalStorage
